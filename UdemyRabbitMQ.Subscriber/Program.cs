25663daa5c16e5e95d12ebdd932b4d044cf013d5
Kamran <311.kamran@gmail.com>
2023-07-31 11:36:07 +0400

    Topic Exchange

--- UdemyRabbitMQ.Subscriber/Program.cs
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -135,6 +135,51 @@
 /// Direct Exchange
 
 
+// using System.Text;
+// using RabbitMQ.Client;
+// using RabbitMQ.Client.Events;
+// 
+// class Program
+// {
+// 	static void Main(string[] args)
+// 	{
+// 		var factory = new ConnectionFactory();
+// 		factory.Uri = new Uri("amqps://fmlpbokz:f8hq9qko1fLnHM_dUC324pRqFNCwQsl2@moose.rmq.cloudamqp.com/fmlpbokz");
+// 
+// 		using var connection = factory.CreateConnection();
+// 
+// 		var channel = connection.CreateModel();
+// 
+// 
+// 		channel.BasicQos(0, 1, false);
+// 		var consumer = new EventingBasicConsumer(channel);
+// 
+// 		var queueName = "direct-queue-Critical";
+// 		channel.BasicConsume(queueName, false, consumer);
+// 
+// 		Console.WriteLine("Loglar dinleniyor...");
+// 
+// 		consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+// 		{
+// 			var message = Encoding.UTF8.GetString(e.Body.ToArray());
+// 
+// 			Thread.Sleep(1500);
+// 			Console.WriteLine("Gelen mesaj: " + message);
+// 
+// 			File.AppendAllText("log-critical.txt", message + "\n");
+// 
+// 			channel.BasicAck(e.DeliveryTag, false);
+// 		};
+// 
+// 		Console.ReadLine();
+// 	}
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Topic Exchange
+
+
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -147,15 +192,21 @@ class Program
 		factory.Uri = new Uri("amqps://fmlpbokz:f8hq9qko1fLnHM_dUC324pRqFNCwQsl2@moose.rmq.cloudamqp.com/fmlpbokz");
 
 		using var connection = factory.CreateConnection();
-
 		var channel = connection.CreateModel();
-
-
 		channel.BasicQos(0, 1, false);
+
 		var consumer = new EventingBasicConsumer(channel);
 
-		var queueName = "direct-queue-Critical";
+		var queueName = channel.QueueDeclare().QueueName;
+		var routeKey = "*.Error.*";
+
+		// Yeni burda deyirikki, evveli Info olsun, axri ne olur olsun
+		// var routeKey = "Info.#";
+
+
+		channel.QueueBind(queueName, "logs-topic", routeKey);
 		channel.BasicConsume(queueName, false, consumer);
+
 
 		Console.WriteLine("Loglar dinleniyor...");
 
@@ -165,8 +216,6 @@ class Program
 
 			Thread.Sleep(1500);
 			Console.WriteLine("Gelen mesaj: " + message);
-
-			File.AppendAllText("log-critical.txt", message + "\n");
 
 			channel.BasicAck(e.DeliveryTag, false);
 		};
