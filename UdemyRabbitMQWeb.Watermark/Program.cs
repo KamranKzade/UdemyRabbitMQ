64ab7dd802f8ab6b4da9bf4a3ae4daaeea4abc98
Kamran <311.kamran@gmail.com>
2023-08-01 14:06:30 +0400

    net5.0 ----> net6.0

--- UdemyRabbitMQWeb.Watermark/Program.cs
+++ UdemyRabbitMQWeb.Watermark/Program.cs
@@ -1,26 +1,54 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using System;
+using RabbitMQ.Client;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQWeb.Watermark.Models;
+using Microsoft.Extensions.Configuration;
+using UdemyRabbitMQWeb.Watermark.Services;
+using Microsoft.Extensions.DependencyInjection;
+using UdemyRabbitMQWeb.Watermark.BackgroundServices;
 
-namespace UdemyRabbitMQWeb.Watermark
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var conn = builder.Configuration.GetConnectionString("RabbitMQ");
+builder.Services.AddSingleton(sp => new ConnectionFactory()
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+	Uri = new Uri(conn),
+	DispatchConsumersAsync = true
+});
+builder.Services.AddSingleton<RabbitMQPublisher>();
+builder.Services.AddSingleton<RabbitMQClientService>();
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
-	}
+// ImageWatermark -i projecte elave edirik
+builder.Services.AddHostedService<ImageWatermarkProcessBackgroundService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseInMemoryDatabase(databaseName: "productDb");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
