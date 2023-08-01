2c29dfd0c053256ebe83b7ff392bee1bc4141810
Kamran <311.kamran@gmail.com>
2023-08-01 17:03:38 +0400

    Adim 2 --> AccountController > LogIn sayfasini olusdurulmasi

---
+++ UdemyRabbitMQWeb.ExcelCreate/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace UdemyRabbitMQWeb.ExcelCreate.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(string Email, string password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(Email);
+
+            if (hasUser == null)
+            {
+                return View();
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(hasUser, password, true, false);
+
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ UdemyRabbitMQWeb.ExcelCreate/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyRabbitMQWeb.ExcelCreate.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
